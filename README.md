50a45038d3028d7e0842cc422cea9d3813522f65
reiger101 <reiger@me.com>
2022-07-08 08:32:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# the-way-station-j826wa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/the-way-station-j826wa)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, ServerComponent, ServersComponent, WarningComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
